ef11d25482a500ff5ae93273b2f86288d7684597
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-06-12 07:09:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('.target')
+}
+
+function deepestChild(){
+  var divGrandNode = document.getElementById('grand-node')
+  var noOfDivs = divGrandNode.querySelectorAll('div').length
+  
+  for(var i = 0; i < noOfDivs; i++){
+    var current = divGrandNode.querySelectorAll('div')[i]
+  }
+  return current
+}
+
+function increaseRankBy(n){
+  var allLi = document.querySelectorAll('.ranked-list li')
+  
+  for(var i = 0; i < allLi.length; i++){
+    allLi[i].innerHTML =  parseInt(allLi[i].innerHTML) + parseInt(n)
+  }
+  return allLi
+}
